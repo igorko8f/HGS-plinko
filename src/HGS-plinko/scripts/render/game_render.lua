d2382a6a246c4b473103bc9583dee3788bd028dc
NobodyI <igorash177@gmail.com>
2026-09-14 23:21:58 +0300

    fixed render context for getting resolution

--- src/HGS-plinko/scripts/render/game_render.lua
+++ src/HGS-plinko/scripts/render/game_render.lua
@@ -1,11 +1,14 @@
 local M = {}
 
-function M.viewport_size()
-    return render.get_width(), render.get_height()
+function M.default_viewport_size()
+    local width = tonumber(sys.get_config("display.width"))
+    local height = tonumber(sys.get_config("display.height"))
+    
+    return width, height
 end
 
 function M.root_position()
-    local width, height = M.viewport_size()
+    local width, height = M.default_viewport_size()
 
     return vmath.vector3(width * 0.5, height * 0.5, 0)
 end
