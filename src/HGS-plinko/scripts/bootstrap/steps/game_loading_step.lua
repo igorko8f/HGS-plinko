7831c7005b9232f11a63dce6a099858ebec1ee68
NobodyI <igorash177@gmail.com>
2026-09-15 22:19:08 +0300

    added gui button

--- src/HGS-plinko/scripts/bootstrap/steps/game_loading_step.lua
+++ src/HGS-plinko/scripts/bootstrap/steps/game_loading_step.lua
@@ -11,6 +11,8 @@ end
 function M.handle_message(context, message_id)
 
     if message_id == hash("proxy_loaded") then
+        
+        msg.post("/gameProxy#collectionproxy", "acquire_input_focus")
         msg.post("/gameProxy", "enable")
         context.bootstrap_complete(true)
     end
