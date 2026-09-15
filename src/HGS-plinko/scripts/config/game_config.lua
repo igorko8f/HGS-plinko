80a259f77b5ab7ce49f77c9e7fa5ea4845b15e4f
NobodyI <igorash177@gmail.com>
2026-09-15 13:00:27 +0300

    setup test configuration

--- src/HGS-plinko/scripts/config/game_config.lua
+++ src/HGS-plinko/scripts/config/game_config.lua
@@ -1,15 +1,56 @@
 local M = {}
 
-M.initial_balls = 12
-M.regeneration = {
-    enabled = true,
-    interval = 0.75,
-}
+M.initial_balls = 10
+M.regeneration_interval = 1
+M.regeneration_balls_count = 3
 M.basket_count = 4
+
+M.baskets = {
+    {
+        probability = 0.5,
+        score = 10
+    },
+    {
+        probability = 0.4,
+        score = 20
+    },
+    {
+        probability = 0.3,
+        score = 30
+    },
+    {
+        probability = 0.2,
+        score = 40
+    },
+    {
+        probability = 0.1,
+        score = 50
+    },
+    {
+        probability = 0.1,
+        score = 50
+    },
+    {
+        probability = 0.2,
+        score = 40
+    },
+    {
+        probability = 0.3,
+        score = 30
+    },
+    {
+        probability = 0.4,
+        score = 20
+    },
+    {
+        probability = 0.5,
+        score = 10
+    }
+}
+
 M.board = {
-    name = "classic",
-    rows = 8,
-    columns = 7,
+    rows = 9,
+    columns = 9,
     width = 560,
     height = 780,
 }
